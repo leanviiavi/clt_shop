a945481a3b0d652e2b9a2482f4c40249a8744057
Andrey Lekhman <leanviiavi@gmail.com>
2025-07-03 12:54:17 +0500

    added domain

--- clt/settings.py
+++ clt/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-ij+1rj1lv5r98ph)=cqn^&@8$^4ii^$zzxnrr*-z(a$=sy#s)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "http://cltshop.kz",
-    "https://cltshop.kz"
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
